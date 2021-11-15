e68ca699698df16b2e60e97148ecb947a508139a
Raul Blanco <rf-blanco1140@users.noreply.github.com>
2021-11-15 17:22:54 +0100

    Player movement with walk, sneak and run

--- Echoes/Assets/Scripts/S_PlayerMovement.cs
+++ Echoes/Assets/Scripts/S_PlayerMovement.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class S_PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum MovementSpeed { Sneak, Walk, Run};
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _sneakSpeed;
+    [SerializeField] private float _runSpeed;
+    private float _movementSpeed;
+
+    private Vector3 _movementVector;
+    private Rigidbody _rb;
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _movementSpeed = _walkSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _movementVector.x = Input.GetAxis("Horizontal");
+        _movementVector.z = Input.GetAxis("Vertical");
         
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            SetMovementSpeed(MovementSpeed.Sneak);
+        }
+        else if(Input.GetKeyDown(KeyCode.L))
+        {
+            SetMovementSpeed(MovementSpeed.Run);
+        }
+        
+        if(Input.GetKeyUp(KeyCode.L) || Input.GetKeyUp(KeyCode.K))
+        {
+            SetMovementSpeed(MovementSpeed.Walk);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(_movementVector.x != 0 || _movementVector.z != 0)
+        {
+            MovePlayerCharacter();
+        }
+    }
+
+    private void MovePlayerCharacter()
+    {
+        _rb.MovePosition(_rb.position + _movementVector * _movementSpeed * Time.deltaTime);
+    }
+
+    private void SetMovementSpeed(MovementSpeed pSpeed)
+    {
+        switch (pSpeed)
+        {
+            case MovementSpeed.Sneak:
+                _movementSpeed = _sneakSpeed;
+                break;
+            case MovementSpeed.Run:
+                _movementSpeed = _runSpeed;
+                break;
+            case MovementSpeed.Walk:
+                _movementSpeed = _walkSpeed;
+                break;
+        }
     }
 }
