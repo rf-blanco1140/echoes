de19e6b72081b9e6f5d79c4508e6110fa6873364
fragileMind <90679956+fragileMind@users.noreply.github.com>
2021-11-15 16:24:14 +0100

    Test Scene Update

--- Echoes/Assets/Scripts/Guanting_Test/TestMovement.cs
+++ Echoes/Assets/Scripts/Guanting_Test/TestMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class TestMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x*Time.deltaTime*moveSpeed
+            + transform.forward * z*Time.deltaTime*moveSpeed;
+
+        transform.position += move;
     }
 }
