1c8dea1a4721f7538c613bdc0f0e82d83635fa56
Raul Blanco <rf-blanco1140@users.noreply.github.com>
2021-11-17 16:57:53 +0100

    Basic monster AI (without damage)

---
+++ Echoes/Assets/Scripts/S_MonsterAI_Basic.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class S_MonsterAI_Basic : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Transform _player;
+
+    //Patroling
+    [SerializeField] Transform[] _walkPoints;
+    private Vector3 _currentWalkPoint;
+    private int _currentWalkPointID;
+    private Vector3 _distanceToWalkPoint;
+
+    //Attacking
+    [SerializeField] private float _distanceFromPlayer;
+    [SerializeField] private float _timeBetweenAttacks;
+    private bool _alreadyAtatcked;
+
+    //States
+    [SerializeField] private float _sightRange;
+    private bool _playerInRange;
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player").transform;
+        _currentWalkPoint = _walkPoints[0].position;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, _player.position);
+        _playerInRange = distance <= _distanceFromPlayer;
+
+        if(!_playerInRange)
+        {
+            Patrolling();
+        }
+        else
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patrolling()
+    {
+        _agent.SetDestination(_currentWalkPoint);
+        _distanceToWalkPoint = transform.position - _currentWalkPoint;
+        
+        if(_distanceToWalkPoint.magnitude < 2f)
+        {
+            SwitchWalkPoint();
+        }
+    }
+
+    private void SwitchWalkPoint()
+    {
+        _currentWalkPointID = _currentWalkPointID == 0 ? 1 : 0;
+        _currentWalkPoint = _walkPoints[_currentWalkPointID].position;
+    }
+
+    private void AttackPlayer()
+    {
+        _agent.SetDestination(_player.position);
+    }
+}
